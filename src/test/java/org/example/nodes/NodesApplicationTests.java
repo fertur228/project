6754fe982b0a718d7712878e79dc0881ee4f4626
fertur228 <ferturferturovich@gmail.com>
2025-04-05 01:51:58 +0500

    Created project

---
+++ src/test/java/org/example/nodes/NodesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
