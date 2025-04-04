269335ee20057fc991f5479b4f7984d45caaad2a
fertur228 <ferturferturovich@gmail.com>
2025-04-05 01:57:42 +0500

    Implemented UserService

---
+++ src/main/java/org/example/nodes/service/UserService.java
@@ -0,0 +1,31 @@
+package org.example.nodes.service;
+
+import org.example.nodes.dto.UserRegistrationRequest;
+import org.example.nodes.model.User;
+import org.example.nodes.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerUser(UserRegistrationRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Пользователь уже существует");
+        }
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setName(request.getName());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        userRepository.save(user);
+    }
+}
