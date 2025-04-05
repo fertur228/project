24a68fe6523dc786be85d2702dfdb67cbf294a8a
fertur228 <ferturferturovich@gmail.com>
2025-04-06 01:56:41 +0500

    the email and password fields have been moved to a separate request class

---
+++ src/main/java/org/example/nodes/dto/UserLoginRequest.java
@@ -0,0 +1,13 @@
+package org.example.nodes.dto;
+
+public class UserLoginRequest {
+    private String email;
+    private String password;
+
+    // Геттеры и сеттеры
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
