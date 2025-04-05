88fbfe8a3d9d6c6019c8f28305b06e355d7f2991
fertur228 <ferturferturovich@gmail.com>
2025-04-06 02:01:25 +0500

    added /login endpoint with return of user information

--- src/main/java/org/example/nodes/controller/AuthController.java
+++ src/main/java/org/example/nodes/controller/AuthController.java
@@ -1,10 +1,17 @@
 package org.example.nodes.controller;
 
-import org.example.nodes.dto.UserRegistrationRequest;
-import org.example.nodes.service.UserService;
 import jakarta.validation.Valid;
+import org.example.nodes.dto.UserLoginRequest;
+import org.example.nodes.dto.UserRegistrationRequest;
+import org.example.nodes.model.User;
+import org.example.nodes.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,5 +26,22 @@ public class AuthController {
     public ResponseEntity<String> registerUser(@Valid @RequestBody UserRegistrationRequest request) {
         userService.registerUser(request);
         return ResponseEntity.ok("Пользователь зарегистрирован!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody UserLoginRequest request) {
+        try {
+            User user = userService.authenticateUser(request.getEmail(), request.getPassword());
+
+            // Можно вернуть только нужные поля
+            return ResponseEntity.ok().body(Map.of(
+                    "id", user.getId(),
+                    "email", user.getEmail(),
+                    "name", user.getName(),
+                    "role", user.getRole().name()
+            ));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(401).body(e.getMessage());
+        }
     }
 }
