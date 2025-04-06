16e9f95501dace352960c71f9aa296baba05e1e2
fertur228 <ferturferturovich@gmail.com>
2025-04-06 21:51:22 +0500

    create PostRepository interface for database operations

---
+++ src/main/java/org/example/nodes/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package org.example.nodes.repository;
+
+import org.example.nodes.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
