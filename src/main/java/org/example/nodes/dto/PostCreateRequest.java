72cd6b885b7cdfdafe71609b636d360e17e1199a
fertur228 <ferturferturovich@gmail.com>
2025-04-06 21:51:51 +0500

    add PostCreateRequest DTO for creating posts

---
+++ src/main/java/org/example/nodes/dto/PostCreateRequest.java
@@ -0,0 +1,12 @@
+package org.example.nodes.dto;
+
+public class PostCreateRequest {
+    private Long authorId;
+    private String content;
+
+    public Long getAuthorId() { return authorId; }
+    public void setAuthorId(Long authorId) { this.authorId = authorId; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
