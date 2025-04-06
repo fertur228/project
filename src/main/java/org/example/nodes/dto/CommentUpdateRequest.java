b66d4252e231c319a14024ecfb3c6b946fb0cbb3
fertur228 <ferturferturovich@gmail.com>
2025-04-06 21:54:19 +0500

    add CommentUpdateRequest DTO for updating comments

---
+++ src/main/java/org/example/nodes/dto/CommentUpdateRequest.java
@@ -0,0 +1,9 @@
+// CommentUpdateRequest.java
+package org.example.nodes.dto;
+
+public class CommentUpdateRequest {
+    private String content;
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
