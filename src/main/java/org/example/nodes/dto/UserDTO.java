f85dd4fcb9928f00190189920fc23aedfb56a956
fertur228 <ferturferturovich@gmail.com>
2025-04-10 21:51:50 +0500

    implement a UserDTO

---
+++ src/main/java/org/example/nodes/dto/UserDTO.java
@@ -0,0 +1,52 @@
+package org.example.nodes.dto;
+
+import org.example.nodes.model.Role;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private Role role;
+
+    // Конструктор
+    public UserDTO(Long id, String name, String email, Role role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    // Геттеры и сеттеры
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
