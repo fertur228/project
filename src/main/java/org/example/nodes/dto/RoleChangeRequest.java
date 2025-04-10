c0b64211f803e007a97237007186b0825046862f
fertur228 <ferturferturovich@gmail.com>
2025-04-10 21:53:53 +0500

    implement a RoleChangeRequest

---
+++ src/main/java/org/example/nodes/dto/RoleChangeRequest.java
@@ -0,0 +1,14 @@
+package org.example.nodes.dto;
+
+public class RoleChangeRequest {
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
+
