b3498a6fa0b6c8f50d9be67dc1a24a6fa5611620
fertur228 <ferturferturovich@gmail.com>
2025-05-02 18:28:45 +0500

    added new endpoint @PostMapping for adding avatar

--- src/main/java/org/example/nodes/controller/UserController.java
+++ src/main/java/org/example/nodes/controller/UserController.java
@@ -1,11 +1,11 @@
-// UserController.java
 package org.example.nodes.controller;
 
-import org.example.nodes.model.User;
+import org.example.nodes.dto.UserDTO;
 import org.example.nodes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,7 +14,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @Autowired // или через конструктор
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -23,7 +23,18 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         return userService.getUserById(id)
-                .map(user -> ResponseEntity.ok().body(user))
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    // POST /api/users/{id}/avatar
+    @PostMapping("/{id}/avatar")
+    public ResponseEntity<UserDTO> uploadAvatar(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+        try {
+            UserDTO userDTO = userService.uploadAvatar(id, file);
+            return ResponseEntity.ok(userDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
