c99916dc70948b310e452f1a87935a55651d7f3d
fertur228 <ferturferturovich@gmail.com>
2025-05-07 02:45:56 +0500

    implement request class for BannedWord function

---
+++ src/main/java/org/example/nodes/dto/BannedWordRequest.java
@@ -0,0 +1,16 @@
+package org.example.nodes.dto;
+
+public class BannedWordRequest {
+    private String word;
+
+
+    // Геттеры и сеттеры
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
+
